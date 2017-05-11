9fd0f17989af7a97e4b65e25f0895c30f537db43
tdubovik <travisd@web.de>
2017-05-11 10:25:50 +0200

    added Seeds for potential Test Data

--- database/seeds/AppointmentsTableSeeder.php
+++ database/seeds/AppointmentsTableSeeder.php
@@ -11,6 +11,24 @@ class AppointmentsTableSeeder extends Seeder
      */
     public function run()
     {
-        //todo
+        DB::table('appointments')->insert([
+            'account_id' = 1,
+            'description' = 'das ist ein Test-Termin.',
+            'active' = true,
+            'weekdays' = 'Wed',
+            'date' = '2017-05-17',
+            'time_from' = '13:00:00',
+            'time_to' = '13:25:00',
+        ])
+            
+        DB::table('appointments')->insert([
+            'account_id' = 2,
+            'description' = 'das ist auch ein Test-Termin.',
+            'active' = true,
+            'weekdays' = 'Sun',
+            'date' = '2017-05-17',
+            'time_from' = '11:00:00',
+            'time_to' = '12:00:00',
+        ])
     }
 }
