9e696e1bc558d228c339356631e0ebb337d139df
tdubovik <travisd@web.de>
2017-05-16 15:07:47 +0200

    inserted test Seeds

--- database/seeds/UserTokensTableSeeder.php
+++ database/seeds/UserTokensTableSeeder.php
@@ -11,6 +11,16 @@ class UserTokensTableSeeder extends Seeder
      */
     public function run()
     {
-        //todo
+        DB::table('user_tokens')->insert([
+           'account_id' => 4,
+           'ttl_in_sec' => 6000,
+            'hash' => md5('testhash'),
+        ]);
+        
+        DB::table('user_tokens')->insert([
+           'account_id' => 5,
+           'ttl_in_sec' => 6000,
+           'hash' => md5('hashtest'),
+        ]);
     }
 }
