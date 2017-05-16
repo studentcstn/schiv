7785d63e21a3de4d694b1ae9335a29ec3022f6f5
tdubovik <travisd@web.de>
2017-05-16 15:24:01 +0200

    Syntax

--- database/seeds/AppointmentsTableSeeder.php
+++ database/seeds/AppointmentsTableSeeder.php
@@ -12,23 +12,23 @@ class AppointmentsTableSeeder extends Seeder
     public function run()
     {
         DB::table('appointments')->insert([
-            'account_id' = 1,
-            'description' = 'das ist ein Test-Termin.',
-            'active' = true,
-            'weekdays' = 'Wed',
-            'date' = '2017-05-17',
-            'time_from' = '13:00:00',
-            'time_to' = '13:25:00',
+            'account_id' => 1,
+            'description' => 'das ist ein Test-Termin.',
+            'active' => true,
+            'weekdays' => 'Wed',
+            'date' => '2017-05-17',
+            'time_from' => '13:00:00',
+            'time_to' => '13:25:00',
         ]);
             
         DB::table('appointments')->insert([
-            'account_id' = 2,
-            'description' = 'das ist auch ein Test-Termin.',
-            'active' = true,
-            'weekdays' = 'Sun',
-            'date' = '2017-05-17',
-            'time_from' = '11:00:00',
-            'time_to' = '12:00:00',
+            'account_id' => 2,
+            'description' => 'das ist auch ein Test-Termin.',
+            'active' => true,
+            'weekdays' => 'Sun',
+            'date' => '2017-05-17',
+            'time_from' => '11:00:00',
+            'time_to' => '12:00:00',
         ]);
     }
 }
