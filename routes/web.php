43f88f729f4357f44ca7309886a4adcf0fbbb2d4
Christian <christian.deinlein@hof-university.de>
2017-05-18 10:52:01 +0200

    first try for routes (not ready)

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,23 @@
 Route::get('/', function () {
     return view('index');
 });
+
+Route::post('login', function () {});
+
+Route::post('register', function () {});
+
+Route::get('register/{token}', function ($token) {});
+
+Route::get('dozenten', function () {});
+
+Route::post('appointment/inscribe', 'InscribeController');
+
+Route::post('appointment/set', 'AppointmentController');
+Route::delete('appointment/{appointment_id}', 'AppointmentController');
+
+Route::get('appointment/{user_id}', //todo);
+Route::get('appointment/{user_id}/past', //todo);
+Route::get('appointment/{user_id}/past/{from}/{to}', //todo);
+
+Route::get('settings/{user_id}', //todo);
+Route::put('settings', //todo);
