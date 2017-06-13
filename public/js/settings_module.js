03c938382ac675e4c1a30c71bcee5c2a43a2d852
Travis James Dubovik <tdubovik@hof-university.de>
2017-06-13 16:00:57 +0200

    added settings_controller

---
+++ public/js/settings_module.js
@@ -0,0 +1,17 @@
+schiv_module.controller('settings_controller', function($scope, $http){
+	//	$http.get('http://localhost/{user id}/settings').
+	//	then(function(response){
+	//	success
+	//	}, function(response){
+	//		failed
+	//		};
+}
+
+schiv_module.controller('edit_settings_controller', function($scope, $http){
+	//	$http.put('http://localhost/{user id}/settings', [fauclties, password, email]).
+	//	then(function(response){
+	//	success
+	//	}, function(response){
+	//		failed
+	//		};
+}
