b407e011fbca44eff1e01b965c13861d3fcc6b90
Travis James Dubovik <tdubovik@fb-ls006-16l.hof-university.de>
2017-06-13 14:36:00 +0200

    added errorcb docent_module and added login_module

---
+++ public/js/login_module.js
@@ -0,0 +1,10 @@
+var login_module = angular.module('login', []);
+
+login_module.controller('login_controller', function($scope, $hhtp){
+	//	$http.post('http://localhost/login',[email, password]).
+	//	then(function(response) {
+	//	success
+	//	}, function(response){
+	//		failed
+	//		};
+}
