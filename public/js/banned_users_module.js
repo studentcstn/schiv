5493ac6ee9f24f2531642c62413e9454dd27c857
Travis James Dubovik <tdubovik@hof-university.de>
2017-06-13 16:10:53 +0200

    added banned_users_module

---
+++ public/js/banned_users_module.js
@@ -0,0 +1,26 @@
+schiv_module.controller('get_banned_users_controller', function($scope, $http){
+	//	$http.get('http://localhost/{id}/banned_users').
+	//	then(function(response){
+	//	success
+	//	}, function(response){
+	//		failed
+	//		};
+}
+
+schiv_module.controller('add_banned_user_controller', function($scope, $http){
+	//	$http.post('http://localhost/{id}/banned_user', [id]).
+	//	then(function(response){
+	//	success
+	//	}, function(response){
+	//		failed
+	//		};
+}
+
+schiv_module.controller('remove_banned_user_controller', function($scope, $http){
+	//	$http.get('http://localhost/{id}/banned_users/{id} ').
+	//	then(function(response){
+	//	success
+	//	}, function(response){
+	//		failed
+	//		};
+}
