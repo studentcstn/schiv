ea1ce1f9c7ee582f253b38e52a96939efbc9c2fd
Christian Deinlein <christian.deinlein@hof-university.de>
2017-06-13 16:54:20 +0200

    use settings_controller

--- public/js/navigation_module.js
+++ public/js/navigation_module.js
@@ -4,17 +4,17 @@ schiv_module.controller("navigation_controller", function ($scope) {
     $scope.show_settings = false;
 
 
-    $scope.index = function () {
+    $scope.to_index = function () {
         $scope.show_index = true;
         $scope.show_past = false;
         $scope.show_settings = false;
     };
-    $scope.past = function () {
+    $scope.to_past = function () {
         $scope.show_index = false;
         $scope.show_past = true;
         $scope.show_settings = false;
     };
-    $scope.settings = function () {
+    $scope.to_settings = function () {
         $scope.show_index = false;
         $scope.show_past = false;
         $scope.show_settings = true;
--- public/js/settings_module.js
+++ public/js/settings_module.js
@@ -1,11 +1,13 @@
 schiv_module.controller('settings_controller', function($scope, $http){
-	//	$http.get('http://localhost/{user id}/settings').
-	//	then(function(response){
-	//	success
-	//	}, function(response){
-	//		failed
-	//		};
-}
+    $http.get('/'+ user_id +'/settings').
+        then(function(response){
+            console.log("ok");
+            $scope.user_settings = response.data;
+            }, function(response){
+            console.log(response.data);
+            $scope.user_settings = {"email":"max","account_faculties":[{"id":"3","name":"Wirtschaft"}],"faculties":[{"id":"1","name":"Informatik"},{"id":"2","name":"Ingenieur"},{"id":"3","name":"Wirtschaft"}]}
+        });
+})
 
 schiv_module.controller('edit_settings_controller', function($scope, $http){
 	//	$http.put('http://localhost/{user id}/settings', [fauclties, password, email]).
@@ -14,4 +16,4 @@ schiv_module.controller('edit_settings_controller', function($scope, $http){
 	//	}, function(response){
 	//		failed
 	//		};
-}
+})
