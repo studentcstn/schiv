5d1fce9b800aa90301fd96ead54e15b2af46cb74
Travis James Dubovik <tdubovik@hof-university.de>
2017-06-13 15:10:34 +0200

    added appointment_signup_controller

---
+++ public/js/appointment_signup_controller.js
@@ -0,0 +1,9 @@
+schiv_module.controller('appointment_signup_controller', function($scope, $hhtp){
+	//	$http.post('http://localhost/{user_id}/appointment_request',[description,
+	//	subject, duration_in_min, request_at, appointment_id]).
+	//	then(function(response){
+	//	success
+	//	}, function(response){
+	//		failed
+	//		};
+}
