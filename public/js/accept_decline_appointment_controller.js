cee3e065c34bb967c27e989025451cae263b508a
Travis James Dubovik <tdubovik@hof-university.de>
2017-06-13 15:22:19 +0200

    added accept_decline_appointment_controller

---
+++ public/js/accept_decline_appointment_controller.js
@@ -0,0 +1,8 @@
+schiv_module.controller('accept_decline_appointment_controller', function($scope, $hhtp){
+	//	$http.put('http://localhost/{id}/appointment_request',[id, state]).
+	//	then(function(response){
+	//	success
+	//	}, function(response){
+	//		failed
+	//		};
+}
