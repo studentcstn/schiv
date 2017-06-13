4c014cf3c092705649a21b79247c610662443cab
Travis James Dubovik <tdubovik@hof-university.de>
2017-06-13 14:56:31 +0200

    added register_module

---
+++ public/js/register_module.js
@@ -0,0 +1,15 @@
+schiv_module.controller('register_controller', function($scope, $hhtp){
+	//	$http.post('http://localhost/register',[email, password]).
+	//	then(function(response){
+	//	success
+	//	}, function(response){
+	//		failed
+	//		};
+	//
+	//	$http.put('http://localhoast/register',[token]).
+	//	then(function(response){
+	//	success
+	//	}, function(response){
+	//		failed
+	//		};
+}
