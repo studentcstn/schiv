004b67595117d78e6bc8b437f0f41957ce8f00d1
Christian Deinlein <christiandeinlein@googlemail.com>
2017-06-20 18:26:33 +0200

    ueberarbeitung login controller

--- public/js/login_controller.js
+++ public/js/login_controller.js
@@ -1,30 +1,33 @@
 schiv_module.controller('login_controller', function($scope, $http){
+    $scope.login = function($scope, $http){
+        //	$http.post('/login',[email, password]).
+        //	then(function(response){
+        //	success
+        //	}, function(response){
+        //		failed
+        //		};
+    };
 
+    $scope.register = function($scope, $http){
+        //	$http.post('/register',[email, password]).
+        //	then(function(response){
+        //	success
+        //	}, function(response){
+        //		failed
+        //		};
+    };
+
+    $scope.confirmRegistration = function($scope, $http){
+        //	$http.put('/register',[token]).
+        //	then(function(response){
+        //	success
+        //	}, function(response){
+        //		failed
+        //		};
+    };
 });
 
-tryLogin = function($scope, $http){
-	//	$http.post('/login',[email, password]).
-	//	then(function(response){
-	//	success
-	//	}, function(response){
-	//		failed
-	//		};
-}
 
-register = function($scope, $http){
-	//	$http.post('/register',[email, password]).
-	//	then(function(response){
-	//	success
-	//	}, function(response){
-	//		failed
-	//		};
-}
 
-confirmRegistration = function($scope $http){
-	//	$http.put('/register',[token]).
-	//	then(function(response){
-	//	success
-	//	}, function(response){
-	//		failed
-	//		};
-}
+
+
