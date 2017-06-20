142146bd4f7aad0599ce67f346527ff418aa358d
Christian Deinlein <christian.deinlein@hof-university.de>
2017-06-20 15:36:35 +0200

    add event window

--- public/js/accept_decline_appointment_controller.js
+++ public/js/accept_decline_appointment_controller.js
@@ -1,3 +1,4 @@
+//todo do in appointment_controller
 schiv_module.controller('accept_appointment_controller', function($scope, $http){
 	//	$http.put('http://localhost/{id}/appointment_request',[id, state]).
 	//	then(function(response){
@@ -6,7 +7,7 @@ schiv_module.controller('accept_appointment_controller', function($scope, $http)
 	//		failed
 	//		};
 });
-
+//todo do in appointment_controller
 schiv_module.controller('decline_appointment_controller', function($scope, $http){
 	//	$http.delete('http://localhost/{id}/appointment_request/{id}').
 	//	then(function(response){
