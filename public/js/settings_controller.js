e76fb87d04d7d045b55dc9143238b06585f33c51
Christian Deinlein <christian.deinlein@hof-university.de>
2017-06-21 09:59:39 +0200

    get settings by use settings button

--- public/js/settings_controller.js
+++ public/js/settings_controller.js
@@ -1,28 +1,7 @@
 schiv_module.controller('settings_controller', function($scope, $http){
-    $scope.settings = function() {
-        $http.get('/' + user_id + '/settings')
-            .then(function (response) {
-                console.log(response.data);
-                $scope.user_settings = response.data;
-                $scope.work($scope);
-            }, function (response) {
-            });
-    };
-    
-    work = function ($scope) {
-        var account_faculties = $scope.user_settings.account_faculties;
-        var faculties = $scope.user_settings.faculties;
-        for (var i = 0; i < faculties.length; ++i) {
-            for (var j = 0; j < account_faculties.length; ++j) {
-                if (faculties[i].id === account_faculties[j].id) {
-                    faculties[i].is_active = true;
-                    break;
-                } else {
-                    faculties[i].is_active = false;
-                }
-            }
-        }
-    };
+    $scope.on("user_settings", function (event, args) {
+       $scope.user_settings = args;
+    });
     
     $scope.saveSettings = function($scope, $http){
     	//	$http.put('/'+ user_id +'/settings', [faculties, password, email]).
