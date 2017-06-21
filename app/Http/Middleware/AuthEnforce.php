6f86752d73e0e9ce2a44c57965fa3b289b921c38
serztle <serztle@online.de>
2017-06-21 15:08:11 +0200

    Save auth in session

--- app/Http/Middleware/AuthEnforce.php
+++ app/Http/Middleware/AuthEnforce.php
@@ -5,7 +5,9 @@ use Closure;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
-class AuthOnceBasic {
+use App\Account;
+
+class AuthEnforce {
     /**
      * Handle an incoming request.
      *
@@ -14,6 +16,18 @@ class AuthOnceBasic {
      * @return mixed
      */
     public function handle($request, Closure $next) {
+        var_dump($request->session()->all());
+
+        $id = $request->session()->get('login_account_id');
+
+        if ($id) {
+            $account = Account::find($id);
+            if ($account) {
+                Auth::login($account);
+                return $next($request);
+            }
+        }
+
         $header = $request->header();
         if (!isset($header['authorization'][0])) {
             // NOTE Debug
@@ -33,6 +47,7 @@ class AuthOnceBasic {
         $email = $emailpassword[0];
         $password = $emailpassword[1];
         if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            $request->session()->put('login_account_id', Auth::user()->id);
             return $next($request);
         } else {
             // NOTE Debug
