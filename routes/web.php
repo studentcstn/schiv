1a00e55abbd9f302688a3aa367c935f283627130
ndiadowitz <nicolas.diadowitz@hof-university.de>
2017-06-21 11:46:22 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,19 @@ Route::put('register', 'RegisterController@update');
 Route::get('docents', 'DocentsController@index');
 Route::get('docents/{docent_id}', 'DocentsController@show');
 
-Route::get('{user_id}/appointment_request', 'AppointmentRequestController@show');
-Route::post('{user_id}/appointment_request', 'AppointmentRequestController@store');
-Route::put('{docent_id}/appointment_request', 'AppointmentRequestController@update');
-Route::delete('{user_id}/appointment_request/{request_id}', 'AppointmentRequestController@destroy');
-
 Route::middleware(['auth.once.basic'])->group(function () {
     Route::post('login', function() {});
+    
+    Route::get('appointment_request', 'AppointmentRequestController@show');
+    Route::post('appointment_request', 'AppointmentRequestController@store');  
+    Route::delete('appointment_request/{request_id}', 'AppointmentRequestController@destroy');
+    
     Route::get('settings', 'SettingsController@show');
     Route::put('settings', 'SettingsController@update');
 });
 Route::middleware(['auth.once.basic'])->group(function() {
+    Route::put('appointment_request', 'AppointmentRequestController@update');
+    
     Route::get('banned_users', 'BannedUsersController@show');
     Route::post('banned_users', 'BannedUsersController@store');
     Route::delete('banned_users/{user_id}', 'BannedUsersController@destroy');
