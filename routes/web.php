1026e163358115cecc37801f6aa7f23b11101010
ndiadowitz <nicolas.diadowitz@hof-university.de>
2017-06-21 13:07:38 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,19 @@ Route::middleware(['auth.once.basic'])->group(function () {
     Route::get('appointment_request', 'AppointmentRequestController@show');
     Route::post('appointment_request', 'AppointmentRequestController@store');
     Route::delete('appointment_request/{request_id}', 'AppointmentRequestController@destroy');
-
+    
     Route::get('settings', 'SettingsController@show');
     Route::put('settings', 'SettingsController@update');
 });
 Route::middleware(['auth.once.basic','auth.docent'])->group(function() {
     Route::put('appointment_request', 'AppointmentRequestController@update');
 
+    Route::get('appointment', 'AppointmentRequestController@show');
+    Route::get('appointment/{count}', 'AppointmentRequestController@show_count');
+    Route::get('appointment/{from}/{to}', 'AppointmentRequestController@show_from_to');
+    Route::post('appointment', 'AppointmentRequestController@store');
+    Route::delete('appointment/{appointment_id}', 'AppointmentRequestController@desroy');
+    
     Route::get('banned_users', 'BannedUsersController@show');
     Route::post('banned_users', 'BannedUsersController@store');
     Route::delete('banned_users/{user_id}', 'BannedUsersController@destroy');
