1793c59a2f56e5de60dc26169597a7aa2ec6bce5
serztle <serztle@online.de>
2017-06-23 16:45:30 +0200

    Fixed reset password

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -2,14 +2,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Config;
 
 use App\Account;
 
 class ResetPasswordController extends Controller {
     public function reset(Request $request) {
         $this->validate($request, [
-            'email' => 'email|required',
-            'password' => 'required|min:10'
+            'email' => 'email|required'
         ]);
         $account = Account::where('email', $request->input('email'))
             ->first();
@@ -18,12 +19,20 @@ class ResetPasswordController extends Controller {
             return response()->json([], 404);
         }
 
+        $newPassword = str_random(10);
         $account->active = false;
-        $account->password = $request->input('password');
+        $account->password = Hash::make($newPassword);
         $account->save();
 
-        return response()->json([
-            'token' => $this->generateToken($account)
-        ]);
+        $debug = Config::get('app.debug');
+
+        if ($debug) {
+            return response()->json([
+                'token' => $this->generateToken($account),
+                'password' => $newPassword,
+            ]);
+        } else {
+            return response()->json([]);
+        }
     }
 }
