ec743e6b2c8331be139dad9ca5726fe52dcd9965
Christian Deinlein <christian.deinlein@hof-university.de>
2017-06-23 09:08:17 +0200

    update index site for student and docent

--- public/js/controller/index_controller.js
+++ public/js/controller/index_controller.js
@@ -1,15 +1,23 @@
 schiv_module.controller('index_controller', function($scope, $http, $rootScope) {
 
-    $scope.$on("show_index", function () {
-        if (user.type === "Docent") {
-            $rootScope.$broadcast("show", "show_index_docent");
-            appointment_request.getAppointments($http, $rootScope, "index_appointment_s", "index_appointment_f");
-        } else {
-
-        }
+    $scope.$on("login_success", function () {
+       show();
     });
 
+    $scope.$on("show_index", function () {
+        show();
+    });
+
+    var show = function () {
+        if (user.type === "Docent") {
+            docent();
+        } else {
+            student();
+        }
+    };
+
     var student = function () {
+        $scope.type = "Student";
         $rootScope.$broadcast("hide", "show_index_docent");
         docent.getDocentList($http, $rootScope, "index_docents_s", "index_docents_f");
         appointment_request.getAppointments($http, $rootScope, "index_appointment_s", "index_appointment_f");
@@ -28,5 +36,16 @@ schiv_module.controller('index_controller', function($scope, $http, $rootScope) 
     });
 
 
+    var docent = function () {
+        $scope.type = "Docent";
+        $rootScope.$broadcast("show", "show_index_docent");
+        appointment_request.getAppointments($http, $rootScope, "index_appointment_s", "index_appointment_f");
+    };
+    $scope.$on("index_appointment_s", function (event, data) {
+
+    });
+    $scope.$on("index_appointment_f", function (event, data) {
+        $rootScope.$broadcast("alert", "warning", data.statusText);
+    });
 
 });
