4f7858a114b18024c996fd6fbb215cd065acff53
Christian Deinlein <christian.deinlein@hof-university.de>
2017-06-24 15:49:32 +0200

    update past

--- public/js/controller/past_controller.js
+++ public/js/controller/past_controller.js
@@ -11,13 +11,13 @@ schiv_module.controller('past_controller', function($scope, $http, $rootScope) {
 
 
     var student = function () {
-        appointment_request.getAppointmentRequest($http, $rootScope, "past_appointment_s", "past_appointment_f");
+        appointment_request.getAppointmentRequestCount($http, $rootScope, "past_appointment_s", "past_appointment_f", 20);
     };
     var docent = function () {
         appointment.getLastAppointments($http, $rootScope, "past_appointment_s", "past_appointment_f");
     };
     $scope.$on("past_appointment_s", function (event, data) {
-
+        $scope.appointments = data;
     });
     $scope.$on("past_appointment_s", function (event, data) {
         $rootScope.$broadcast("alert", "warning", data.statusText);
--- public/js/model/appointment_request.js
+++ public/js/model/appointment_request.js
@@ -1,45 +1,55 @@
 appointment_request = {
-		acceptAppointmentRequest: function($http, $rootScope, broadcastSuccess, broadcastFailed, id, state){
-	        $http.put('/appointment_requests', {"id": id, "state": state})
-	        .then(function(response){
-	        	console.log(response);
-	        	$rootScope.$broadcast(broadcastSuccess, response);
-	        }, function(response){
-	        	console.log(response);
-	        	$rootScope.$broadcast(broadcastFailed, response);
-	        });
-		},
-		
-		declineAppointmentRequest: function($http, $rootScope, broadcastSuccess, broadcastFailed, appointment_request_id){
-	        $http.delete('/appointment_requests/' + appointment_request_id)
-	            .then(function(response){
-	            	console.log(response);
-	            	$rootScope.$broadcast(broadcastSuccess, response);
-	            }, function(response){
-	            	console.log(response);
-	            	$rootScope.$broadcast(broadcastFailed, response);
-	            });
-		},
-		
-		createAppointmentRequest: function($http, $rootScope, broadcastSuccess, broadcastFailed, description, subject, appointment_id, date){
-	        $http.post('/appointment_requests',{"description": description, "subject": subject, "appointment_id": appointment_id, "date": date})
-	            .then(function(response){
-	            	console.log(response);
-	            	$rootScope.$broadcast(broadcastSuccess, response);
-	            }, function(response) {
-	            	console.log(response);
-	            	$rootScope.$broadcast(broadcastFailed, response);
-	            });
-		},
-		
-		getAppointmentRequest: function($http, $rootScope, broadcastSuccess, broadcastFailed){
-			$http.get('appointment_requests')
-				.then(function(response){
-	            	console.log(response);
-	            	$rootScope.$broadcast(broadcastSuccess, response.data);
-				}, function(response){
-	            	console.log(response);
-	            	$rootScope.$broadcast(broadcastFailed, response);
-				});
-		}
+    acceptAppointmentRequest: function($http, $rootScope, broadcastSuccess, broadcastFailed, id, state){
+        $http.put('/appointment_requests', {"id": id, "state": state})
+        .then(function(response){
+            console.log(response);
+            $rootScope.$broadcast(broadcastSuccess, response);
+        }, function(response){
+            console.log(response);
+            $rootScope.$broadcast(broadcastFailed, response);
+        });
+    },
+
+    declineAppointmentRequest: function($http, $rootScope, broadcastSuccess, broadcastFailed, appointment_request_id){
+        $http.delete('/appointment_requests/' + appointment_request_id)
+            .then(function(response){
+                console.log(response);
+                $rootScope.$broadcast(broadcastSuccess, response);
+            }, function(response){
+                console.log(response);
+                $rootScope.$broadcast(broadcastFailed, response);
+            });
+    },
+
+    createAppointmentRequest: function($http, $rootScope, broadcastSuccess, broadcastFailed, description, subject, appointment_id, date){
+        $http.post('/appointment_requests',{"description": description, "subject": subject, "appointment_id": appointment_id, "date": date})
+            .then(function(response){
+                console.log(response);
+                $rootScope.$broadcast(broadcastSuccess, response);
+            }, function(response) {
+                console.log(response);
+                $rootScope.$broadcast(broadcastFailed, response);
+            });
+    },
+
+    getAppointmentRequest: function($http, $rootScope, broadcastSuccess, broadcastFailed){
+        $http.get('appointment_requests')
+            .then(function(response){
+                console.log(response);
+                $rootScope.$broadcast(broadcastSuccess, response.data);
+            }, function(response){
+                console.log(response);
+                $rootScope.$broadcast(broadcastFailed, response);
+            });
+    },
+    getAppointmentRequestCount: function($http, $rootScope, broadcastSuccess, broadcastFailed, count){
+        $http.get('appointment_requests/' + count)
+            .then(function(response){
+                console.log(response);
+                $rootScope.$broadcast(broadcastSuccess, response.data);
+            }, function(response){
+                console.log(response);
+                $rootScope.$broadcast(broadcastFailed, response);
+            });
+    }
 }
