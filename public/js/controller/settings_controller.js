44359dfc2d00dcdb3472114c0a16f8620caba046
Christian Deinlein <christiandeinlein@googlemail.com>
2017-06-25 13:51:41 +0200

    update save settings

--- public/js/controller/settings_controller.js
+++ public/js/controller/settings_controller.js
@@ -31,17 +31,34 @@ schiv_module.controller('settings_controller', function($scope, $http, $rootScop
         error(data);
     });
 
+    var saved = 1;
 
     $scope.save = function () {
+        saved = 1;
         settings.saveSettings($http, $rootScope, "settings_save_s", "settings_save_f", $scope.newUser_settings.email, $scope.newUser_settings.password, $scope.newUser_settings.faculties);
-        if (user.type == "Docent")
+        if (user.type == "Docent") {
+            ++saved;
             ban.unbanAccount($http, $rootScope, "settings_unbun_s", "settings_unbun_f", $scope.ban);
+        }
     };
-    $scope.$on("settings_save_s", function (event, data) {
-        $rootScope.$broadcast("alert", "success", "Changes saved.")
-        setting();
+    $scope.$on("settings_save_s", function () {
+        --saved;
+        if (saved == 0) {
+            $rootScope.$broadcast("alert", "success", "Changes saved.");
+            setting();
+        }
     });
     $scope.$on("settings_save_f", function (event, data) {
+        error(data);
+    });
+    $scope.$on("settings_unbun_s", function () {
+        --saved;
+        if (saved == 0) {
+            $rootScope.$broadcast("alert", "success", "Changes saved.");
+            setting();
+        }
+    });
+    $scope.$on("settings_unbun_f", function (event, data) {
         error(data);
     });
 
