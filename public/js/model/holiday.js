8ade5995755340e5ecfcf48f67bf91fc6bd31daa
tdubovik <travisd@web.de>
2017-06-25 18:27:52 +0200

    modified holiday.js model

--- public/js/model/holiday.js
+++ public/js/model/holiday.js
@@ -1,45 +1,78 @@
 holiday ={
+
 		getHolidays: function($http, $rootScope, broadcastSuccess, broadcastFailed, from, to){
-	        $http.get('/holidays/' + from + '/' + to +'')
-            .then(function(response) {
-                console.log(response);
-                $rootScope.$broadcast(broadcastSuccess, response.data);
-            }, function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastFailed, response);
-            });
-		}
+			connection.lock(function(){
+				get_Holidays($http, $rootScope, broadcastSuccess, broadcastFailed, from, to);
+			});
+		},
 
 		createHolidays: function($http, $rootScope, broadcastSuccess, broadcastFailed, from, to, name){
-			$http.post('/holidays', {"from:" from, "to": to, "name": name})
-			.then(function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastSuccess, response);
-			}, function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastFailed, response);
+			connection.lock(function(){
+				create_Holidays($http, $rootScope, broadcastSuccess, broadcastFailed, from, to, name);
 			});
-		}
-		
+		},
+
 		editHoliday: function($http, $rootScope, broadcastSuccess, broadcastFailed, from, to, name, id){
-			$http.put('/holidays', {"from": from, "to": to, "name": name, "id": id})
-			.then(function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastSuccess, response);
-			}, function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastFailed, response);
+			connection.lock(function(){
+				edit_Holiday($http, $rootScope, broadcastSuccess, broadcastFailed, from, to, name, id);
 			});
-		}
+		},
 		
 		deleteHoliday: function($http, $rootScope, broadcastSuccess, broadcastFailed, id){
-			$http.put('/holidays/' + id +'')
-			.then(function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastSuccess, response);
-			}, function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastFailed, response);
+			connection.lock(function(){
+				delete_Holiday($http, $rootScope, broadcastSuccess, broadcastFailed, id);
 			});
 		}
-}
+};
+
+var get_Holidays = function($http, $rootScope, broadcastSuccess, broadcastFailed, from, to){
+    $http.get('/holidays/' + from + '/' + to +'')
+    .then(function(response) {
+    	connection.free();
+        console.log(response);
+        $rootScope.$broadcast(broadcastSuccess, response.data);
+    }, function(response){
+    	connection.free();
+        console.log(response);
+        $rootScope.$broadcast(broadcastFailed, response);
+    });
+};
+
+var create_Holidays: function($http, $rootScope, broadcastSuccess, broadcastFailed, from, to, name){
+	$http.post('/holidays', {"from:" from, "to": to, "name": name})
+	.then(function(response){
+		connection.free();
+        console.log(response);
+        $rootScope.$broadcast(broadcastSuccess, response);
+	}, function(response){
+		connection.free();
+        console.log(response);
+        $rootScope.$broadcast(broadcastFailed, response);
+	});
+};
+
+var edit_Holiday = function($http, $rootScope, broadcastSuccess, broadcastFailed, from, to, name, id){
+	$http.put('/holidays', {"from": from, "to": to, "name": name, "id": id})
+	.then(function(response){
+		connection.free();
+        console.log(response);
+        $rootScope.$broadcast(broadcastSuccess, response);
+	}, function(response){
+		connection.free();
+        console.log(response);
+        $rootScope.$broadcast(broadcastFailed, response);
+	});
+};
+
+var delete_Holiday = function($http, $rootScope, broadcastSuccess, broadcastFailed, id){
+	$http.put('/holidays/' + id +'')
+	.then(function(response){
+		connection.free();
+        console.log(response);
+        $rootScope.$broadcast(broadcastSuccess, response);
+	}, function(response){
+		connection.free();
+        console.log(response);
+        $rootScope.$broadcast(broadcastFailed, response);
+	});
+};
