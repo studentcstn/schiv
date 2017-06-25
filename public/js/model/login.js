a3005f0e4a38e37a47b594b8fd1083969865ee98
tdubovik <travisd@web.de>
2017-06-25 18:12:38 +0200

    modified login.js model

--- public/js/model/login.js
+++ public/js/model/login.js
@@ -1,56 +1,101 @@
 login = {
-    login: function ($http, $rootScope, broadcastSuccess, broadcastFailed, email, password) {
-        $http.put('/login', {email: email, password: password})
-            .then(function (response) {
-                console.log(response);
-                var d = response.data.account;
-                d.email = d.email.substring(0, d.email.indexOf('@'));
-                d.name = d.email.substring(0, d.email.indexOf('.'));
-                d.last = d.email.substring(d.email.indexOf('.')+1);
-                $rootScope.$broadcast(broadcastSuccess, response.data);
-            }, function (response) {
-                console.log(response);
-                $rootScope.$broadcast(broadcastFailed, response);
-            });
-    },
-    register: function($http, $rootScope, broadcastSuccess, broadcastFailed, email, password) {
-        $http.post('/register',{"email": email, "password": password})
-            .then(function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastSuccess, response.data);
-            }, function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastFailed, response);
-            });
-    },
-    confirmRegistration: function($http, $rootScope, broadcastSuccess, broadcastFailed, token){
-        $http.put('/register', {token: token})
-            .then(function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastSuccess, response.data);
-            }, function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastFailed, response);
-            });
-    },
-    forgotPassword: function($http, $rootScope, broadcastSuccess, broadcastFailed, email) {
-        $http.put('/reset', {"email": email})
-            .then(function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastSuccess, response.data);
-            }, function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastFailed, response);
-            });
-    },
-    logout: function ($http, $rootScope, broadcastSuccess, broadcastFailed) {
-        $http.put('/logout', {})
-            .then(function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastSuccess, response.data);
-            }, function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastFailed, response);
-            });
-    }
+		
+		login: function($http, $rootScope, broadcastSuccess, broadcastFailed, email, password){
+			connection.lock(function(){
+				login_($http, $rootScope, broadcastSuccess, broadcastFailed, email, password);
+			});
+		},
+
+		register: function($http, $rootScope, broadcastSuccess, broadcastFailed, email, password){
+			connection.lock(function(){
+				register_($http, $rootScope, broadcastSuccess, broadcastFailed, email, password);
+			});
+		},
+
+		 confirmRegistration: function($http, $rootScope, broadcastSuccess, broadcastFailed, token){
+			connection.lock(function(){
+				confirm_Registration($http, $rootScope, broadcastSuccess, broadcastFailed, token);
+			});
+		 },
+
+		 forgotPassword: function($http, $rootScope, broadcastSuccess, broadcastFailed, email){
+				connection.lock(function(){
+					forgot_Password($http, $rootScope, broadcastSuccess, broadcastFailed, email);
+				});
+		 },
+
+		 logout: function ($http, $rootScope, broadcastSuccess, broadcastFailed){
+				connection.lock(function(){
+					 logout_($http, $rootScope, broadcastSuccess, broadcastFailed);
+				}); 
+		 }
+};
+
+var login_ = function ($http, $rootScope, broadcastSuccess, broadcastFailed, email, password) {
+    $http.put('/login', {email: email, password: password})
+        .then(function (response) {
+        	connection.free();
+            console.log(response);
+            var d = response.data.account;
+            d.email = d.email.substring(0, d.email.indexOf('@'));
+            d.name = d.email.substring(0, d.email.indexOf('.'));
+            d.last = d.email.substring(d.email.indexOf('.')+1);
+            $rootScope.$broadcast(broadcastSuccess, response.data);
+        }, function (response) {
+        	connection.free();
+            console.log(response);
+            $rootScope.$broadcast(broadcastFailed, response);
+        });
+};
+
+var register_ = function($http, $rootScope, broadcastSuccess, broadcastFailed, email, password) {
+    $http.post('/register',{"email": email, "password": password})
+        .then(function(response){
+        	connection.free();
+            console.log(response);
+            $rootScope.$broadcast(broadcastSuccess, response.data);
+        }, function(response){
+        	connection.free();
+            console.log(response);
+            $rootScope.$broadcast(broadcastFailed, response);
+        });
+};
+
+var confirm_Registration = function($http, $rootScope, broadcastSuccess, broadcastFailed, token){
+    $http.put('/register', {token: token})
+        .then(function(response){
+        	connection.free();
+            console.log(response);
+            $rootScope.$broadcast(broadcastSuccess, response.data);
+        }, function(response){
+        	connection.free();
+            console.log(response);
+            $rootScope.$broadcast(broadcastFailed, response);
+        });
+};
+
+var forgot_Password: function($http, $rootScope, broadcastSuccess, broadcastFailed, email) {
+    $http.put('/reset', {"email": email})
+        .then(function(response){
+        	connection.free();
+            console.log(response);
+            $rootScope.$broadcast(broadcastSuccess, response.data);
+        }, function(response){
+        	connection.free();
+            console.log(response);
+            $rootScope.$broadcast(broadcastFailed, response);
+        });
+};
+
+var logout_ = function ($http, $rootScope, broadcastSuccess, broadcastFailed) {
+    $http.put('/logout', {})
+        .then(function(response){
+        	connection.free();
+            console.log(response);
+            $rootScope.$broadcast(broadcastSuccess, response.data);
+        }, function(response){
+        	connection.free();
+            console.log(response);
+            $rootScope.$broadcast(broadcastFailed, response);
+        });
 };
