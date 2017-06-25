377e559689695680347642c304b02b109aeae4c1
tdubovik <travisd@web.de>
2017-06-25 18:18:15 +0200

    modified settings.js model

--- public/js/model/settings.js
+++ public/js/model/settings.js
@@ -1,39 +1,17 @@
 settings = {
-    getSettings: function ($http, $rootScope, broadcastSuccess, broadcastFailed) {
-        $http.get('/settings')
-            .then(function (response) {
-                console.log(response);
-                var d = response.data;
-                d.email = d.email.substring(0, d.email.indexOf('@'));
-                work(response.data);
-                $rootScope.$broadcast(broadcastSuccess, response.data);
-            }, function (response) {
-                console.log(response);
-                $rootScope.$broadcast(broadcastFailed, response);
-            });
-    },
-    saveSettings: function($http, $rootScope, broadcastSuccess, broadcastFailed, email, password, faculties){
-        var settings = {};
-        if (email != null && email != "")
-            settings.email = email+"@hof-university.de";
-        if (password != null && password != "")
-            settings.password = password;
-        var faculty = [];
-        for (i = 0; i < faculties.length; ++i)
-            if (faculties[i].active == true)
-                faculty.push(faculties[i].id);
-        if (faculty.length > 0)
-            settings.faculties = faculty;
-        console.log(settings);
-        $http.put('/settings', settings)
-            .then(function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastSuccess, response.data);
-            }, function(response){
-                console.log(response);
-                $rootScope.$broadcast(broadcastFailed, response);
-            });
-    }
+		
+	getSettings: function ($http, $rootScope, broadcastSuccess, broadcastFailed){
+		connection.lock(function(){
+			get_Settings($http, $rootScope, broadcastSuccess, broadcastFailed);
+		});
+	},
+		
+	saveSettings: function($http, $rootScope, broadcastSuccess, broadcastFailed, email, password, faculties){
+		connection.lock(function(){
+			save_Settings: function($http, $rootScope, broadcastSuccess, broadcastFailed, email, password, faculties)
+		});
+	}
+
 };
 
 var work = function (user_settings) {
@@ -49,4 +27,45 @@ var work = function (user_settings) {
             }
         }
     }
+};
+
+var get_Settings = function ($http, $rootScope, broadcastSuccess, broadcastFailed) {
+    $http.get('/settings')
+        .then(function (response) {
+        	connection.free();
+            console.log(response);
+            var d = response.data;
+            d.email = d.email.substring(0, d.email.indexOf('@'));
+            work(response.data);
+            $rootScope.$broadcast(broadcastSuccess, response.data);
+        }, function (response) {
+        	connection.free();
+            console.log(response);
+            $rootScope.$broadcast(broadcastFailed, response);
+        });
+};
+
+var save_Settings = function($http, $rootScope, broadcastSuccess, broadcastFailed, email, password, faculties){
+    var settings = {};
+    if (email != null && email != "")
+        settings.email = email+"@hof-university.de";
+    if (password != null && password != "")
+        settings.password = password;
+    var faculty = [];
+    for (i = 0; i < faculties.length; ++i)
+        if (faculties[i].active == true)
+            faculty.push(faculties[i].id);
+    if (faculty.length > 0)
+        settings.faculties = faculty;
+    console.log(settings);
+    $http.put('/settings', settings)
+        .then(function(response){
+        	connection.free();
+            console.log(response);
+            $rootScope.$broadcast(broadcastSuccess, response.data);
+        }, function(response){
+        	connection.free();
+            console.log(response);
+            $rootScope.$broadcast(broadcastFailed, response);
+        });
 };
