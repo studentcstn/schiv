78168f641f3fef9072db87bc56bea9944b0a070c
tdubovik <travisd@web.de>
2017-06-25 16:34:47 +0200

    merge appointments prototype in appointment.js model

--- public/js/model/appointment.js
+++ public/js/model/appointment.js
@@ -59,6 +59,16 @@ appointment = {
     },
 
     merge_appointments: function (appointments, appointment_requests) {
-
+    	for(n = 0; n<appointments.length; ++i){
+    		//appointment_requests_loop = [];
+    		//counter = 0;
+    		for(i = 0; i<appointment_requests.length; ++i){
+    		//if(appointments[n].id == appointment_requests[i].appointment_id){
+    		//	appointment_request_loop[counter] = appointment_request[i];
+    		//	counter++;
+    		//}
+    		}
+    		//appointments[n].appointment_requests = appointment_requests_loop
+    	}
     }
 };
