9bf9ed2289f3740b5f410a30d3dc765b4358ee22
Christian Deinlein <christiandeinlein@googlemail.com>
2017-06-26 07:57:02 +0200

    improvment create appointment

--- public/js/controller/appointment_controller.js
+++ public/js/controller/appointment_controller.js
@@ -9,7 +9,7 @@ schiv_module.controller('appointment_controller', function($scope, $http, $rootS
             ],
         day: ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"],
         date: "(0?[1-9]|[12][0-9]|3[01])[.](0?[1-9]|1[0-2])[.][0-9]{4}",
-        time: "[012]?[0-9][:][0-5][0-9]"
+        time: "([0]?[0-9]|1[0-2])[:][0-5][0-9]"
     };
 
     $scope.newAppointment = {day: ""};
@@ -20,7 +20,7 @@ schiv_module.controller('appointment_controller', function($scope, $http, $rootS
     };
 
     $scope.create = function () {
-        appointment.createAppointment($http, $rootScope, "create_s", "create_f", $scope.newAppointment.day, $scope.newAppointment.time_from, $scope.newAppointment.time_to, $scope.newAppointment.description)
+        appointment.createAppointment($http, $rootScope, "create_s", "create_f", $scope.newAppointment.day, $scope.newAppointment.from, $scope.newAppointment.to, $scope.newAppointment.description)
     };
     $scope.$on("create_s", function (event, data) {
 
