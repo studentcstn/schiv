a5a3363d3ade3ad4d9b1e42ecc2a47c73a846c0d
Christian Deinlein <christiandeinlein@googlemail.com>
2017-06-26 08:55:19 +0200

    update add appointment

--- public/js/controller/appointment_controller.js
+++ public/js/controller/appointment_controller.js
@@ -8,7 +8,7 @@ schiv_module.controller('appointment_controller', function($scope, $http, $rootS
             {id: 1, name: "singular"}
             ],
         day: ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"],
-        date: "(0?[1-9]|[12][0-9]|3[01])[.](0?[1-9]|1[0-2])[.][0-9]{4}",
+        date: "(0?[1-9]|[12][0-9]|3[01])[.](0?[1-9]|1[0-2])[.]([0-9]{2}|[0-9]{4})",
         time: "([01]?[0-9]|2[0-3])[:][0-5][0-9]"
     };
 
--- public/js/model/appointment.js
+++ public/js/model/appointment.js
@@ -83,6 +83,13 @@ var get_lastAppointmentsFromTo = function($http, $rootScope, broadcastSuccess, b
 var create_appointment = function($http, $rootScope, broadcastSuccess, broadcastFailed, day, time_from, time_to, description){
     time_from = controlTime(time_from);
     time_to = controlTime(time_to);
+    if(!day.match("(FRI|MON|S(AT|UN)|T(UE|HU)|WED)")) {
+        day = controlDay(day);
+        if (!controlDate(day)) {
+            $rootScope.$broadcast(broadcastFailed, "wrong date");
+            return;
+        }
+    }
     $http.post('/appointments',
         {"day": day,"time_from": time_from, "time_to": time_to, "description": description})
         .then(function(response){
@@ -100,6 +107,35 @@ var controlTime = function(time) {
     if (time.length < 5)
         time = "0"+time;
     return time + ":00";
+};
+
+var controlDay = function (day) {
+    var d = day.split('.');
+    if (d[2].length < 4)
+        d[2] = "20"+d[2];
+    if (d[1].length < 2)
+        d[1] = "0"+d[1];
+    if (d[0].length < 2)
+        d[0] = "0"+d[0];
+    return d[2] + "-" + d[1] + "-" + d[0];
+};
+
+var controlDate = function (date) {
+    var d = date.split("-");
+    var leapYear = false;
+
+    if ((d[0]%4 == 0 && !d[0]%100 == 0) || d[0]%400 == 0)
+        leapYear = true;
+
+    if (leapYear && d[1] == 2 && d[2] <= 29)
+        return true;
+    if (!leapYear && d[1] == 2 && d[2] <= 28)
+        return true;
+    if ((d[1] == 1 || d[1] == 3 || d[1] == 5 || d[1] == 7 || d[1] == 8 || d[1] == 10 || d[1] == 12 && d[2]) <= 31)
+        return true;
+    if ((d[1] == 4 || d[1] == 6 || d[1] == 9 || d[1] == 11) && d[2] <= 30)
+        return true;
+    return false;
 };
 
 var delete_appointment = function($http, $rootScope, broadcastSuccess, broadcastFailed, appointment_id){
