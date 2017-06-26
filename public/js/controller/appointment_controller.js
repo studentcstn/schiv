dbb731c2e1cebecb77d7a96e42adf0d9d7167d3a
Christian Deinlein <christiandeinlein@googlemail.com>
2017-06-26 07:37:47 +0200

    update create appointment

--- public/js/controller/appointment_controller.js
+++ public/js/controller/appointment_controller.js
@@ -7,7 +7,7 @@ schiv_module.controller('appointment_controller', function($scope, $http, $rootS
             {id: 0, name: "frequent"},
             {id: 1, name: "singular"}
             ],
-        day: ["Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+        day: ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"],
         date: "(0?[1-9]|[12][0-9]|3[01])[.](0?[1-9]|1[0-2])[.][0-9]{4}",
         time: "[012]?[0-9][:][0-5][0-9]"
     };
