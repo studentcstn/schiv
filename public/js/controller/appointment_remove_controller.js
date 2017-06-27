c5c5bb797560913fe6a729a5621ec912e786124c
Christian Deinlein <christian.deinlein@hof-university.de>
2017-06-27 14:58:48 +0200

    fix remove appointment

--- public/js/controller/appointment_remove_controller.js
+++ public/js/controller/appointment_remove_controller.js
@@ -1,8 +1,9 @@
 schiv_module.controller('remove_appointment_controller', function($scope, $http, $rootScope) {
 
-    $scope.$on('remove_appointment', function (event, id, data) {
+    $scope.$on('remove_appointment', function (event, type, id, data) {
         $scope.id = id;
         $scope.appointment = data;
+        $scope.type = type;
         $rootScope.$broadcast("show", "show_remove_appointment");
     });
 
@@ -14,7 +15,11 @@ schiv_module.controller('remove_appointment_controller', function($scope, $http,
     };
 
     $scope.remove = function () {
-        appointment_request.declineAppointmentRequest($http, $rootScope, "remove_s", "remove_f", $scope.id);
+        if ($scope.type = "Docent") {
+            appointment_request.acceptAppointmentRequest($scope.id, "Declined", 0);
+        } else {
+            appointment_request.declineAppointmentRequest($http, $rootScope, "remove_s", "remove_f", $scope.id);
+        }
     };
     $scope.$on("remove_s", function () {
         $rootScope.$broadcast("hide", "show_remove_appointment");
