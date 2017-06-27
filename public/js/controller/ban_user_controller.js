8aa1ef8d960e38f94a3223b1cee5cbe9497347b3
Travis James Dubovik <tdubovik@hof-university.de>
2017-06-27 19:01:55 +0200

    added ban_user_controller

---
+++ public/js/controller/ban_user_controller.js
@@ -0,0 +1,28 @@
+schiv_module.controller('ban_user_controller', function($scope, $http, $rootScope) {
+	
+	$scope.$on("ban_user", function(event, id){
+		$scope.id = id;
+		$rootScope.$broadcast("show", "show_ban_user");
+	});
+
+	$scope.ban_close = function(){
+		$rootScope.$broadcast("hide","show_ban_user");
+	};
+
+	$scope.ban = function(){
+		ban.banAccount($http, $rootScope, "ban_user_s", "ban_user_f", $scope.id);
+	};
+	
+	$scope.$on("ban_user_s", function(){
+		$rootScope.$broadcast("alert", "success", "succesfully banned user");
+		$rootScope.$broadcast("hide","show_ban_user");
+	});
+	
+	$scope.$on("ban_user_f", function(data){
+		error(data);
+	});
+
+	var error = function(data){
+		$rootScope.$broadcast("error", data);
+	};
+}
