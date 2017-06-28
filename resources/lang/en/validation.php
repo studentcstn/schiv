13e33674d4657fb8f61a0cab68724d89c0a24183
serztle <serztle@online.de>
2017-06-28 17:42:14 +0200

    Added custom validation messages

---
+++ resources/lang/en/validation.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'date_format' => "doesn't match date format ':format'",
+    'required' => "is required",
+    'numeric' => "must be a number",
+    'max' => "length must be at least :max",
+    'min' => "length must be greater than :min",
+    'email' => "not a valid email",
+    // TODO
+    // 'state' => Rule::in(['Accepted', 'Declined']),
+    // 'weekday'=> Rule::in(['MON','TUE','WED','THU','FRI','SAT','SUN','NULL']),
+    // 'date'=> 'required_if:weekday,==,NULL|date_format:Y-m-d',
+];
