5a091e68c2c20d6963d25a71ce61d3015ec8f81d
Christian Deinlein <christian.deinlein@hof-university.de>
2017-06-28 16:24:29 +0200

    update settings

--- public/js/controller/settings_controller.js
+++ public/js/controller/settings_controller.js
@@ -1,7 +1,8 @@
 schiv_module.controller('settings_controller', function($scope, $http, $rootScope){
     $scope.settings = {
         email: "[a-z]+[.][a-z]+[0-9]*",
-        date: "(0?[1-9]|[12][0-9]|3[01])[.](0?[1-9]|1[0-2])[.]([0-9]{2}|[0-9]{4})"
+        date: "(0?[1-9]|[12][0-9]|3[01])[.](0?[1-9]|1[0-2])[.]([0-9]{2}|[0-9]{4})",
+        subject: "[a-zA-Z0-9]{2,}"
     };
 
     $scope.newHoliday = {description: "", time_from: "", time_to: ""};
