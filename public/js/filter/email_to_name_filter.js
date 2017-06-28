3432913a49d21f910796aabe33050fd11447a635
Travis James Dubovik <tdubovik@hof-university.de>
2017-06-28 15:32:39 +0200

    added email_to_name_filter

---
+++ public/js/filter/email_to_name_filter.js
@@ -0,0 +1,15 @@
+schiv_module.filter('email_to_name',function(){
+	return function(email){
+	var dot = email.search(".");
+	for(i = email.length-1, i >=0; --i){
+		if(email.charAt(i).match([0-9]))
+		{
+			email.replace(email.charAt(i)),"");
+		}			
+	}
+	var at = email.search("@");
+	var first_name = email.substring(0,dot);
+	var last_name = email.substring(dot+1,at);
+	return first_name + " " + last_name;
+	}
+}
