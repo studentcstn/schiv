f0a0ddc0760c81cb0ad2ea1b1fe9a3113296550b
Travis James Dubovik <tdubovik@hof-university.de>
2017-06-28 12:29:24 +0200

    added translations

--- public/js/controller/appointment_controller.js
+++ public/js/controller/appointment_controller.js
@@ -8,13 +8,13 @@ schiv_module.controller('appointment_controller', function($scope, $http, $rootS
             {id: 1, name: languages.appointment.singular[language]}
             ],
         day: [
-            {id: "MON", name: "languages.days.monday[language]"},
-            {id: "TUE", name: "languages.days.tuesday[language],"},
-            {id: "WED", name: "languages.days.wednesday[language],"},
-            {id: "THU", name: "languages.days.thursday[language],"},
-            {id: "FRI", name: "languages.days.friday[language],"},
-            {id: "SAT", name: "languages.days.saturday[language],"},
-            {id: "SUN", name: "languages.days.sunday[language]],"}
+            {id: "MON", name: languages.days.monday[language]},
+            {id: "TUE", name: languages.days.tuesday[language]},
+            {id: "WED", name: languages.days.wednesday[language]},
+            {id: "THU", name: languages.days.thursday[language]},
+            {id: "FRI", name: languages.days.friday[language]},
+            {id: "SAT", name: languages.days.saturday[language]},
+            {id: "SUN", name: languages.days.sunday[language]]}
             ],
         date: "(0?[1-9]|[12][0-9]|3[01])[.](0?[1-9]|1[0-2])[.]([0-9]{2}|[0-9]{4})",
         time: "([01]?[0-9]|2[0-3])[:][0-5][0-9]"
@@ -26,13 +26,13 @@ schiv_module.controller('appointment_controller', function($scope, $http, $rootS
             {id: 1, name: languages.appointment.singular[language]}
         ];
         $scope.appointment_build.day = [
-            {id: "MON", name: "languages.days.monday[language]"},
-            {id: "TUE", name: "languages.days.tuesday[language],"},
-            {id: "WED", name: "languages.days.wednesday[language],"},
-            {id: "THU", name: "languages.days.thursday[language],"},
-            {id: "FRI", name: "languages.days.friday[language],"},
-            {id: "SAT", name: "languages.days.saturday[language],"},
-            {id: "SUN", name: "languages.days.sunday[language]],"}
+            {id: "MON", name: languages.days.monday[language]},
+            {id: "TUE", name: languages.days.tuesday[language]},
+            {id: "WED", name: languages.days.wednesday[language]},
+            {id: "THU", name: languages.days.thursday[language]},
+            {id: "FRI", name: languages.days.friday[language]},
+            {id: "SAT", name: languages.days.saturday[language]},
+            {id: "SUN", name: languages.days.sunday[language]]}
         ];
     });
 
