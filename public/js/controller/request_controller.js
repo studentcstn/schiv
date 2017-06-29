7c6ebf58e1c8948cbfa63e2b1a06a3d61c3a47e6
Christian Deinlein <christiandeinlein@googlemail.com>
2017-06-29 19:14:03 +0200

    update add appointment request

--- public/js/controller/request_controller.js
+++ public/js/controller/request_controller.js
@@ -1,6 +1,7 @@
 schiv_module.controller('request_controller', function($scope, $http, $rootScope) {
 
     $scope.$on('show_inscribe', function (event, data) {
+        $scope.dates = {};
         $scope.docent = data;
         $scope.appointments = data.appointments;
         appointment.sort_appointment($scope.appointments);
@@ -23,6 +24,15 @@ schiv_module.controller('request_controller', function($scope, $http, $rootScope
     $scope.$on("request_f", function (event, data) {
         error(data);
     });
+
+    $scope.inscribeSetAppointment = function (item) {
+        $scope.newAppointment_request.id = item.id;
+        $scope.newAppointment_request.date = item.date;
+        $scope.newAppointment_request.time_from = item.time_from;
+        $scope.newAppointment_request.time_to = item.time_to;
+        $scope.newAppointment_request.name = item.description;
+        $scope.dates = item
+    };
 
     var error = function (data) {
         $rootScope.$broadcast("error", data);
