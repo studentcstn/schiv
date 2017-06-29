613506a7cd725aa8455ec1f2ec76f5a269d604b4
ndiadowitz <nicolas.diadowitz@hof-university.de>
2017-06-29 14:42:07 +0200

    Create AppointmentDeleted.php

---
+++ app/Mail/AppointmentDeleted.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class AppointmentDeleted extends Mailable {
+    use Queueable, SerializesModels;
+    
+    private $date;
+    private $time;
+    
+    public function __construct($date, $time) {
+        $this->date= $date;
+        $this->time = $time;
+    }
+    
+    public function build() {
+        return $this->view('emails.appointmentdeleted', [
+            'date'    => $this->date,
+            'time' => $this->time,
+        ]);
+    }
+}
