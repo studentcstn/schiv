ce5c93a9332f1b02edebd5bbdf9d61fb1097828f
ndiadowitz <nicolas.diadowitz@hof-university.de>
2017-06-29 14:46:47 +0200

    Create appointmentdeleted.blade.php

---
+++ resources/views/emails/appointmentdeleted.blade.php
@@ -0,0 +1,5 @@
+Der Termin am {{ $date }} um {{ $time }} Uhr wurde entfernt. Bitte überprüfen Sie ihre Anfragen.
+
+###########################
+
+The appointment at the {{ $date }} at {{ $time }} o'clock has been removed. Please check your requests.
