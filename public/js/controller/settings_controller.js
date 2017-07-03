ac0dfff3e3b69cedbca40ab88f02a0539c08f8c4
Christian Deinlein <christian.deinlein@hof-university.de>
2017-07-03 08:52:51 +0200

    update docent list

--- public/js/controller/settings_controller.js
+++ public/js/controller/settings_controller.js
@@ -2,7 +2,7 @@ schiv_module.controller('settings_controller', function($scope, $http, $rootScop
     $scope.settings = {
         email: "[a-z]+[.][a-z]+[0-9]*",
         date: "((0?[1-9]|[12][0-9]|3[01])[.](0?[13578]|1[02])|(0?[1-9]|[12][0-9]|3[0])[.](0?[469]|11)|([01]?[1-9]|2[0-9])[.](0?[2]))[.]([0-9]{2}|[0-9]{4})",
-        subject: "[a-zA-Z0-9]{2,}"
+        subject: "[^ ]{2,}.*"
     };
 
     $scope.newHoliday = {description: "", time_from: "", time_to: ""};
